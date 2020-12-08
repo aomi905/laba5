bcf099a834a58f207ab3eccb67c8bad1e86e4b44
Anna Suravets <hanna.s905@gmail.com>
2020-12-08 18:48:17 +0300

    Метод сохранения в txt файл

--- src/bsu/rfact/java/laba5/Main.java
+++ src/bsu/rfact/java/laba5/Main.java
@@ -1,8 +1,131 @@
 package bsu.rfact.java.laba5;
 
-public class Main {
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.Iterator;
+import java.util.ArrayList;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+
+public class MainFrame extends JFrame {
+    private static final int WIDTH = 700;
+    private static final int HEIGHT = 500;
+    private JFileChooser fileChooser = null;
+    private JMenuItem resetGraphicsMenuItem;
+    private GraphicsDisplay display = new GraphicsDisplay();
+    private boolean fileLoaded = false;
+    private JMenuItem saveToTextMenuItem;
+    private DecimalFormat formatter = new DecimalFormat("###.#####");
+
+    public MainFrame() {
+        super("Обработка событий от мыши");
+        this.setSize(700, 500);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        this.setLocation((kit.getScreenSize().width - 700) / 2, (kit.getScreenSize().height - 500) / 2);
+        this.setExtendedState(6);
+        JMenuBar menuBar = new JMenuBar();
+        this.setJMenuBar(menuBar);
+        JMenu fileMenu = new JMenu("Файл");
+        menuBar.add(fileMenu);
+        Action openGraphicsAction = new AbstractAction("Открыть файл с графиком") {
+            public void actionPerformed(ActionEvent event) {
+                if (MainFrame.this.fileChooser == null) {
+                    MainFrame.this.fileChooser = new JFileChooser();
+                    MainFrame.this.fileChooser.setCurrentDirectory(new File("."));
+                }
+
+                MainFrame.this.fileChooser.showOpenDialog(MainFrame.this);
+                MainFrame.this.openGraphics(MainFrame.this.fileChooser.getSelectedFile());
+                saveToTextMenuItem.setEnabled(true);
+            }
+        };
+
+        fileMenu.add(openGraphicsAction);
+        Action saveToTextAction = new AbstractAction("Сохранить в текстовый файл") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(fileChooser == null){
+                    fileChooser = new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File("."));
+                }
+                if (fileChooser.showSaveDialog(MainFrame.this) ==
+                        JFileChooser.APPROVE_OPTION){
+                    saveToTextFile(fileChooser.getSelectedFile());
+                }
+            }
+        };
+        saveToTextMenuItem = fileMenu.add(saveToTextAction);
+        saveToTextMenuItem.setEnabled(false);
+
+        Action resetGraphicsAction = new AbstractAction("Отменить все изменения") {
+            public void actionPerformed(ActionEvent event) {
+                MainFrame.this.display.reset();
+            }
+        };
+        this.resetGraphicsMenuItem = fileMenu.add(resetGraphicsAction);
+        this.resetGraphicsMenuItem.setEnabled(false);
+        this.getContentPane().add(this.display, "Center");
+    }
+
+    protected void openGraphics(File selectedFile) {
+        try {
+            DataInputStream in = new DataInputStream(new FileInputStream(selectedFile));
+            ArrayList graphicsData = new ArrayList(50);
+
+            while(in.available() > 0) {
+                Double x = in.readDouble();
+                Double y = in.readDouble();
+                graphicsData.add(new Double[]{x, y});
+            }
+
+            if (graphicsData.size() > 0) {
+                this.fileLoaded = true;
+                this.resetGraphicsMenuItem.setEnabled(true);
+                this.display.displayGraphics(graphicsData);
+            }
+
+        } catch (FileNotFoundException var6) {
+            JOptionPane.showMessageDialog(this, "Указанный файл не найден", "Ошибка загрузки данных", 2);
+        } catch (IOException var7) {
+            JOptionPane.showMessageDialog(this, "Ошибка чтения координат точек из файла", "Ошибка загрузки данных", 2);
+        }
+    }
+
+    private void saveToTextFile(File selectedFile) {
+        try {
+            FileWriter writer = new FileWriter(selectedFile);
+            ArrayList<Double[]> originalData = display.getGraphicsData();
+            Iterator iter = originalData.iterator();
+
+            while(iter.hasNext()) {
+                Double[] point = (Double[])iter.next();
+                for(int i = 0; i < 2; i++){
+                    writer.write(String.valueOf(formatter.format(point[i])));
+                    int a = formatter.format(point[i]).toString().length();
+                    for (int l = 1; l < (30 - a); l++) {
+                        writer.write(" ");
+                    }
+                }
+                writer.write("\n");
+            }
+            writer.flush();
+        }catch (IOException e){
+            System.out.println("File couldn't be created");
+        }
+    }
+
 
     public static void main(String[] args) {
-
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(3);
+        frame.setVisible(true);
     }
 }
